d4fec31dac0ca22a315a82de87a6747e4fc4f018
Anton <rumiant348@yandex.ru>
2023-04-21 18:02:36 +0300

    auf done

--- app/src/main/java/com/example/affirmations/data/Datasource.kt
+++ app/src/main/java/com/example/affirmations/data/Datasource.kt
@@ -15,13 +15,14 @@
  */
 package com.example.affirmations.data
 import com.example.affirmations.R
+import com.example.affirmations.model.Affirmation
 
 /**
  * [Datasource] generates a list of [Affirmation]
  */
 class Datasource() {
-    /*fun loadAffirmations(): List<Affirmation> {
-        return listOf<Affirmation>(
+    fun loadAffirmations(): List<Affirmation> {
+        return listOf(
             Affirmation(R.string.affirmation1, R.drawable.image1),
             Affirmation(R.string.affirmation2, R.drawable.image2),
             Affirmation(R.string.affirmation3, R.drawable.image3),
@@ -31,6 +32,20 @@ class Datasource() {
             Affirmation(R.string.affirmation7, R.drawable.image7),
             Affirmation(R.string.affirmation8, R.drawable.image8),
             Affirmation(R.string.affirmation9, R.drawable.image9),
-            Affirmation(R.string.affirmation10, R.drawable.image10))
-    }*/
+            Affirmation(R.string.affirmation10, R.drawable.image10),
+        )
+    }
+
+    fun loadAuf(): List<Affirmation> {
+        return listOf<Affirmation>(
+            Affirmation(R.string.volk_1, R.drawable.volk_1),
+            Affirmation(R.string.volk_2, R.drawable.volk_2),
+            Affirmation(R.string.volk_3, R.drawable.volk_3),
+            Affirmation(R.string.volk_4, R.drawable.volk_4),
+            Affirmation(R.string.volk_5, R.drawable.volk_5),
+            Affirmation(R.string.volk_6, R.drawable.volk_6),
+            Affirmation(R.string.volk_7, R.drawable.volk_7),
+            Affirmation(R.string.volk_8, R.drawable.volk_8),
+        )
+    }
 }
